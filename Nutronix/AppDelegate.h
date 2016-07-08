3f4911c93539b744b8a90b2a94a6218c8c7effbe
luwo1701 <luke.worley@colorado.edu>
2016-07-08 14:34:41 -0600

    first commit

---
+++ Nutronix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nutronix
+//
+//  Created by Luke Worley on 7/8/16.
+//  Copyright © 2016 LJWProductions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ README.md
@@ -0,0 +1,2 @@
+This Repository contains an IOS version of the personal-nutrition-logger web app I've been working on in Ruby
+The two will eventually be communicating with the same database
