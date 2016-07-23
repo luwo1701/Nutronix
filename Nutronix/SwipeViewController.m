81337047bda895939217d67da751cd9e9002fb18
luwo1701 <luke.worley@colorado.edu>
2016-07-23 13:40:22 -0600

    trying to cycle through images via swipes not currently functional

--- Nutronix/SwipeViewController.m
+++ Nutronix/SwipeViewController.m
@@ -13,12 +13,19 @@
     - (IBAction)rightSwipe:(UISwipeGestureRecognizer *)sender;
     @property (weak, nonatomic) IBOutlet UIImageView *image;
     @property (weak, nonatomic) IBOutlet UILabel *count;
+    @property (weak,nonatomic) NSArray *imageNames;
+    @property NSUInteger index;
 @end
 
 @implementation SwipeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _index = 0;
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"FoodProperties" ofType:@"plist"];
+    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:filePath];
+    _imageNames = dict[@"Images"];
+    _image.image = [UIImage imageNamed:[_imageNames objectAtIndex: _index]];
     // Do any additional setup after loading the view.
 }
 
@@ -40,19 +47,27 @@
 
 
 - (IBAction)rightSwipe:(UISwipeGestureRecognizer *)sender {
-    int index;
-    if (sender.direction==UISwipeGestureRecognizerDirectionLeft){
-        _image.image=[UIImage imageNamed:@"Chicken"];
-        index++;
+    
+    _index++;
+    NSInteger max = [_imageNames count];
+    //NSString *maxString = [NSString  stringWithFormat:@"d", max];
+   // NSLog(maxString);
+    if (_index > [_imageNames count]) {
+        _index = 0;
     }
-    if (sender.direction==UISwipeGestureRecognizerDirectionRight){
-        _image.image=[UIImage imageNamed:@"Beef"];
-        index--;
-    }
-    NSString *counter = [NSString stringWithFormat:@"%d", index];
-    _count.text = counter;
+    _image.image=[UIImage imageNamed:[_imageNames objectAtIndex: _index]];
 }
 
 - (IBAction)leftSwipe:(UISwipeGestureRecognizer *)sender {
+    if (_index != 0) {
+        _index--;
+    }
+    else{
+        _index = [_imageNames count];
+    }
+  
+    _image.image = [UIImage imageNamed:[_imageNames objectAtIndex:_index]];
+    
 }
+
 @end
